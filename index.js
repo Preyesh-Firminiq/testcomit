b4c458346d64836ab8e20f92987a5edd919a1840
Preyesh-Firminiq <preyesh.mishra@firminiq.com>
2021-11-16 18:15:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// "use strict";
+
+// const {promises: {readFile}} = require("fs");
+
+// readFile("test.txt").then(fileBuffer => {
+//   console.log(fileBuffer.toString());
+// }).catch(error => {
+//   console.error(error.message);
+//   process.exit(1);
+// });
+
+"use strict";
+
+const { Console } = require("console");
+const {promises: {readFile}} = require("fs");
+
+Promise.all([
+  readFile("test.txt"),
+  readFile("test32.txt"),
+  readFile("test3.txt")
+]).then(([packageJson, readme, indexjs]) => {
+
+    console.log("All Files readed")
+  console.log(packageJson.toString());
+  console.log(readme.toString());
+  console.log(indexjs.toString());
+}).catch(error => {
+    console.log("Failed to read file !")
+  console.error(error.message);
+  process.exit(1);
+});
